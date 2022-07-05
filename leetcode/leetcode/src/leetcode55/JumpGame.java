fefc5dcfca79ae0e6bb07aef818b8984eb146915
Seph-23 <seongmlyu95@gmail.com>
2022-07-05 15:45:57 +0900

    problem 55

--- leetcode/leetcode/src/leetcode55/JumpGame.java
+++ leetcode/leetcode/src/leetcode55/JumpGame.java
@@ -3,16 +3,19 @@ package leetcode55;
 public class JumpGame {
 
     public static void main(String[] args) {
-        int[] nums = {2, 3, 1, 1, 4};
-
+        int[] nums = {3,0,0,0,5,0,0,0,0,4};
         boolean ans = canJump(nums);
         System.out.println(ans);
     }
 
     static boolean canJump(int[] nums) {
-        boolean ans = false;
-
-
-        return ans;
+        int max = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;        //if iteration runs until the end of the array return true.
     }
 }
