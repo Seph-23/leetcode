0ee66ac510748b6756563e1e0ce3504fb8a2b6d7
Seph-23 <seongmlyu95@gmail.com>
2022-07-05 14:40:27 +0900

    algorithm practice problems

---
+++ leetcode/leetcode/src/leetcode55/JumpGame.java
@@ -0,0 +1,18 @@
+package leetcode55;
+
+public class JumpGame {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+
+        boolean ans = canJump(nums);
+        System.out.println(ans);
+    }
+
+    static boolean canJump(int[] nums) {
+        boolean ans = false;
+
+
+        return ans;
+    }
+}
