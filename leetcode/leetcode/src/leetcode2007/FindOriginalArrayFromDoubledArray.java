e553b41f6cbeaecf6faf596b7d1abb99fe3072e1
Seph-23 <seongmlyu95@gmail.com>
2022-07-11 09:47:54 +0900

    07-11-2022

--- leetcode/leetcode/src/leetcode2007/FindOriginalArrayFromDoubledArray.java
+++ leetcode/leetcode/src/leetcode2007/FindOriginalArrayFromDoubledArray.java
@@ -8,9 +8,9 @@ public class FindOriginalArrayFromDoubledArray {
     public static void main(String[] args) {
 //        int[] changed = {1, 3, 4, 2, 6, 8};       // 1 2 3 4 6 8
 //        int[] changed = {3, 1};
-//        int[] changed = {0, 3, 2, 4, 6, 0}; //0 0 2 3 4 6
+        int[] changed = {0, 3, 2, 4, 6, 0}; //0 0 2 3 4 6
 //        int[] changed = {2, 1};
-        int[] changed = {1};
+//        int[] changed = {1};
 
         changed = findOriginalArray(changed);
         for (int a : changed) {
