3c0392bc553cadb8c8e294ebfaa8aaf63b41935f
Seph-23 <seongmlyu95@gmail.com>
2022-07-12 10:10:26 +0900

    07-12-2022

---
+++ leetcode/leetcode/src/leetcode502/Ipo.java
@@ -0,0 +1,10 @@
+package leetcode502;
+
+public class Ipo {
+    public static void main(String[] args) {
+
+    }
+    static int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+
+    }
+}
