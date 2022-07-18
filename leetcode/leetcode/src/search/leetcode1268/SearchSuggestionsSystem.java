43e7c690b5f5028a0264016d31ff117ca22a03b5
Seph-23 <seongmlyu95@gmail.com>
2022-07-18 10:34:29 +0900

    07-18-2022 10:34

---
+++ leetcode/leetcode/src/search/leetcode1268/SearchSuggestionsSystem.java
@@ -0,0 +1,14 @@
+package search.leetcode1268;
+
+import java.util.List;
+
+public class SearchSuggestionsSystem {
+    public static void main(String[] args) {
+        String[] products = {"mobile","mouse","moneypot","monitor","mousepad"};
+        String searchWord = "mouse";
+    }
+
+//    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+//
+//    }
+}
