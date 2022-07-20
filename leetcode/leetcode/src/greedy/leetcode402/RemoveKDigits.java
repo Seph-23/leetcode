ff6c7864d35dd88443220ecfa5025600c42fd502
Seph-23 <seongmlyu95@gmail.com>
2022-07-20 14:59:41 +0900

    Remove K Digits: How to use Stack.

--- leetcode/leetcode/src/greedy/leetcode402/RemoveKDigits.java
+++ leetcode/leetcode/src/greedy/leetcode402/RemoveKDigits.java
@@ -1,7 +1,10 @@
 package greedy.leetcode402;
 
+import java.util.Stack;
+
 /**
- * 이 문제는 나중에 다시 보자! Stack 사용해서 풀어야 함!
+ * Stack 사용해서 풀어야 하는 문제.
+ * Good Example of Stack Usage.
  */
 public class RemoveKDigits {
     public static void main(String[] args) {
@@ -12,6 +15,39 @@ public class RemoveKDigits {
         System.out.println(ans);
     }
     static String removeKdigits(String num, int k) {
-        return " ";
+        int len = num.length();
+
+        if (k == len) {             //Corner Case: k == num length
+            return "0";
+        }
+
+        Stack<Character> stack = new Stack();
+        int i=0;
+        while (i < num.length()) {
+            //Whenever meet a digit which is less than the previous digit, discard the previous one
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+
+        while (k > 0) {             //Corner case like "1111"
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();                 //Building stack into string
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+
+        while (sb.length() > 1 && sb.charAt(0) == '0') {        //remove all the 0 at the head
+            sb.deleteCharAt(0);
+        }
+
+        return sb.toString();
     }
 }
