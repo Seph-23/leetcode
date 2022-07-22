96acda40cf233db76cb2af9ad833e9c82960d31d
Seph-23 <seongmlyu95@gmail.com>
2022-07-22 10:03:04 +0900

    07-22-2022

--- leetcode/leetcode/src/greedy/leetcode406/QueueReconstructionByHeight.java
+++ leetcode/leetcode/src/greedy/leetcode406/QueueReconstructionByHeight.java
@@ -2,7 +2,7 @@ package greedy.leetcode406;
 
 public class QueueReconstructionByHeight {
     public static void main(String[] args) {
-
+        int[][] people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
     }
 
     public int[][] reconstructQueue(int[][] people) {
