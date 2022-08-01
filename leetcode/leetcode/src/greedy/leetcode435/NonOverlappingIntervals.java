f399de13abca03139ed92581fd62c291160dc07e
Seph-23 <seongmlyu95@gmail.com>
2022-08-01 11:02:12 +0900

    MinimumNumberOfArrowsToBurstBalloons - leetcode452 done, activity search

--- leetcode/leetcode/src/greedy/leetcode435/NonOverlappingIntervals.java
+++ leetcode/leetcode/src/greedy/leetcode435/NonOverlappingIntervals.java
@@ -2,10 +2,15 @@ package greedy.leetcode435;
 
 import java.util.Arrays;
 
+/**
+ * Done
+ */
 public class NonOverlappingIntervals {
 
   public static void main(String[] args) {
-    int[][] intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+//    int[][] intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+//    int[][] intervals = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
+    int[][] intervals = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
     System.out.println(eraseOverlapIntervals(intervals));
   }
 
@@ -13,7 +18,6 @@ public class NonOverlappingIntervals {
     if (intervals == null || intervals.length == 0) {
       return 0;
     }
-
     //sorts intervals in ascending order comparing 2nd element of
     //each array inside intervals
     Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
@@ -27,7 +31,6 @@ public class NonOverlappingIntervals {
         count++;
       }
     }
-
     return n-count;     //return (total arrays - # of arrays not overlapping).
   }
 }
--- leetcode/leetcode/src/greedy/leetcode452/MinimumNumberOfArrowsToBurstBalloons.java
+++ leetcode/leetcode/src/greedy/leetcode452/MinimumNumberOfArrowsToBurstBalloons.java
@@ -2,36 +2,26 @@ package greedy.leetcode452;
 
 import java.util.Arrays;
 
+/**
+ * Done
+ */
 public class MinimumNumberOfArrowsToBurstBalloons {
 
   public static void main(String[] args) {
     int[][] points = {{10, 16}, {2, 8}, {1, 6}, {7, 12}};
-//    int[][] points = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+//    int[][] points = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
     System.out.println(findMinArrowShots(points));
   }
 
   static int findMinArrowShots(int[][] points) {
-    if (points == null || points.length == 0 || points[0].length == 0) {      //corner case
-      return 0;
-    }
-
-    Arrays.sort(points, (a, b) -> a[0] - b[0] == 0 ? a[1] - b[1] : a[0] - b[0]);
-
-    int index = 0 , result = 0;
-
-    while (index < points.length) {
-      int end = points[index][1];
-      while (index + 1 < points.length) {
-        if (end >= points[index + 1][0]) {
-          end = Math.min(end, points[index + 1][1]);
-          index++;
-        }else {
-          break;
-        }
-        result++;
-        index++;
+    Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+    int ans = 0, arrow = 0;
+    for (int i = 0; i < points.length; i++) {
+      if (ans == 0 || points[i][0] > arrow) {
+        ans++;
+        arrow = points[i][1];
       }
     }
-    return result;
+    return ans;
   }
 }
