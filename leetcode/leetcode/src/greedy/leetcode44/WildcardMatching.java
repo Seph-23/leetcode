2cae643e66aa691e7b1e8886771e939e2e67d0f6
Seph-23 <seongmlyu95@gmail.com>
2022-09-23 18:18:07 +0900

    wildcard matching - greedy

---
+++ leetcode/leetcode/src/greedy/leetcode44/WildcardMatching.java
@@ -0,0 +1,32 @@
+package greedy.leetcode44;
+
+public class WildcardMatching {
+
+    public static void main(String[] args) {
+        String s = "aa";
+        String p = "pp";
+        System.out.println(isMatch(s, p));
+    }
+
+    static boolean isMatch(String s, String p) {
+        boolean[][] match=new boolean[s.length()+1][p.length()+1];
+        match[s.length()][p.length()]=true;
+        for(int i=p.length()-1;i>=0;i--){
+            if(p.charAt(i)!='*')
+                break;
+            else
+                match[s.length()][i]=true;
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
+                    match[i][j]=match[i+1][j+1];
+                else if(p.charAt(j)=='*')
+                    match[i][j]=match[i+1][j]||match[i][j+1];
+                else
+                    match[i][j]=false;
+            }
+        }
+        return match[0][0];
+    }
+}
