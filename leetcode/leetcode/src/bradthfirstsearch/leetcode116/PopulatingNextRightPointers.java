caf286cd7b2f5ce8a430e9c294adcef3d553bc18
Seph-23 <seongmlyu95@gmail.com>
2022-12-15 16:34:40 +0900

    feat: Populating Next Right Pointers in Each Node - leetcode116

---
+++ leetcode/leetcode/src/bradthfirstsearch/leetcode116/PopulatingNextRightPointers.java
@@ -0,0 +1,37 @@
+package bradthfirstsearch.leetcode116;
+
+public class PopulatingNextRightPointers {
+  public void connect(TreeLinkNode root) {
+    TreeLinkNode level_start=root;
+    while(level_start!=null){
+      TreeLinkNode cur=level_start;
+      while(cur!=null){
+        if(cur.left!=null) cur.left.next=cur.right;
+        if(cur.right!=null && cur.next!=null) cur.right.next=cur.next.left;
+
+        cur=cur.next;
+      }
+      level_start=level_start.left;
+    }
+  }
+
+  class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left;
+    public TreeLinkNode right;
+    public TreeLinkNode next;
+
+    public TreeLinkNode() {}
+
+    public TreeLinkNode(int _val) {
+      val = _val;
+    }
+
+    public TreeLinkNode(int _val, TreeLinkNode _left, TreeLinkNode _right, TreeLinkNode _next) {
+      val = _val;
+      left = _left;
+      right = _right;
+      next = _next;
+    }
+  };
+}
