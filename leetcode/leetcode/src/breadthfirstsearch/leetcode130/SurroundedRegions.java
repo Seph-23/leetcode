fe99b1be3ded441359d7e5d350f182ce0b3f3c9d
Seph-23 <seongmlyu95@gmail.com>
2022-12-26 17:44:40 +0900

    feat: Added SurroundedRegions solution, leetcode130, breadthfirst-search

---
+++ leetcode/leetcode/src/breadthfirstsearch/leetcode130/SurroundedRegions.java
@@ -0,0 +1,43 @@
+package bradthfirstsearch.leetcode130;
+
+public class SurroundedRegions {
+    public void solve(char[][] board) {
+        if (board.length == 0 || board[0].length == 0) {
+            return;
+        }
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            flush(board, i, 0);
+            flush(board, i, n - 1);
+        }
+        for (int j = 0; j < n; j++) {
+            flush(board, 0, j);
+            flush(board, m - 1, j);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                flip(board, i, j);
+            }
+        }
+    }
+
+    private void flush(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 0;
+        flush(board, i - 1, j);
+        flush(board, i + 1, j);
+        flush(board, i, j + 1);
+        flush(board, i, j - 1);
+    }
+
+    private void flip(char[][] board, int i, int j) {
+        if (board[i][j] == 'O') {
+            board[i][j] = 'X';
+        } else if (board[i][j] == 0) {
+            board[i][j] = 'O';
+        }
+    }
+}
